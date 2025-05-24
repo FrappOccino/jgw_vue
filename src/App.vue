9896772660091e9a07dbe9c30eb7e618a7830c3d
jeffrey wong <wongjeffreg@gmail.com>
2025-05-24 17:24:45 +0800

    updated to composition API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
-<script>
+<script setup>
   // options API
-  export default {
-    data(){
-      return{
-        name: 'jeff',
-        status: 'active',
-        tasks: ['task1', 'task2', 'task3']
-      };
-    },
+  import {ref} from 'vue'
+
+  const name = ref('jeff');
+  const status = ref('active');
+  const tasks = ref(['task1', 'task2', 'task3']);
+  const counter = ref(0);
+
+  function add(){
+    counter.value ++;
   };
 </script>
 
@@ -16,10 +17,14 @@
   <p v-if="status === 'active'"> User is active</p>
   <p v-else-if="status === 'pending'"> User is pending</p>
   <p v-else> User is inactive</p>
-
+  <code> Counter: {{ counter }}</code>
+  <br>
+  <button @click="add">add</button>
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
+
+
 </template>
 
 <style scoped>
