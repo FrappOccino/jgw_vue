76799c8e7bf026f0dc07ebab9c281a13aacd6ff8
Jeffrey Wong <jeffrey.wong@avvanz.com>
2025-05-27 19:40:55 +0800

    integrate metreial UI

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import MyForm from './views/MyForm.vue';
   import Nav from './components/Nav.vue';
   import {RouterView} from 'vue-router';
   import { RouterLink } from 'vue-router';
@@ -19,7 +20,8 @@
   <Nav />
   <!-- Use this when you are using Vue app -->
   <!-- Use <slot /> if using inertia in laravel -->
-  <RouterView /> 
+  <RouterView />
+  <MyForm />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/all.js';
 import './assets/main.css';
 import router from './router';
 
