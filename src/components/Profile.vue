c8f2ec72b4b710ce8c3da3c91c19360ce2927f2f
jeffrey wong <wongjeffreg@gmail.com>
2025-05-28 19:53:03 +0800

    Profile Component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,10 @@
-
 <template>
     <div id="profile">
-
+        <img src="../assets/Wong_ID photo.jpg" alt="picture">
+        <div id="info">
+            <p>Jeffrey Wong</p>
+            <p>Junior Software Engineer</p>
+            <p>Innovation through technology drives my passion and motivation</p>
+        </div>
     </div>
 </template>
